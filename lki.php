6a85e0825fc9125cc12d47b901f8fbb071e10311
Mehdy <100050754+eelx001@users.noreply.github.com>
2024-06-28 09:41:37 +0000

    Create lki.php

---
+++ lki.php
@@ -0,0 +1,12 @@
+<?php
+$myVar = 0;
+/*
+ * test is an function
+ * for testing app
+ **/ 
+function test() {
+	if ($myVar === 0) {
+		return "hello world!"; // say hello for the world please
+	}
+}
+?>
